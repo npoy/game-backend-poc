3121777fcf84f5f2273ff6af3991ef5239b18b11
Nicolas <nicolas.poy@outlook.com>
2021-02-03 23:44:11 -0300

    Added docker and docker-compose

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import "os"
 func main() {
 	a := App{}
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"))
 
 	a.Run(":8010")
 }
